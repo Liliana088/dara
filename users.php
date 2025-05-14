7d95886d6725921b9fdaabd4798bbf845f1aeb06
Liliana23-0291 <lpmdavid23-0291@cca.edu.ph>
2025-05-14 21:13:21 +0800

    added bootstrap fonts,icons offline

--- users.php
+++ users.php
@@ -13,9 +13,9 @@ if (!isset($_SESSION['user_name'])) {
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>User Management</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" />
+    <link href="bootstrap-offline/css/bootstrap.css" rel="stylesheet">
+    <link rel="stylesheet" href="/dara/bootstrap-icons/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="/dara/fontawesome/css/all.min.css" />
     <link href="/dara/css/users.css" rel="stylesheet">
     <link rel="icon" type="image/x-icon" href="img/daraa.ico">
 </head>
@@ -196,7 +196,7 @@ if (!isset($_SESSION['user_name'])) {
     </div>
 
     <!-- Scripts -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="bootstrap-offline/js/bootstrap.bundle.min.js"></script>
     <script>
         // Toggle sidebar function
         function toggleSidebar() {
