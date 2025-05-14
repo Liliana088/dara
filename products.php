dc61f95dd2ef493de5edc5cf65d55e2784c476df
Liliana23-0291 <lpmdavid23-0291@cca.edu.ph>
2025-05-14 21:36:44 +0800

    added jquery and js

--- products.php
+++ products.php
@@ -55,9 +55,9 @@ $totalPages = ceil($totalRows / $itemsPerPage);  // Calculate the total pages
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Product Management</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" />
+    <link href="bootstrap-offline/css/bootstrap.css" rel="stylesheet">
+    <link rel="stylesheet" href="/dara/bootstrap-icons/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="/dara/fontawesome/css/all.min.css" />
     <link rel="icon" type="image/x-icon" href="img/daraa.ico">
     <link href="/dara/css/products.css" rel="stylesheet" />
   </head>
@@ -305,8 +305,8 @@ $totalPages = ceil($totalRows / $itemsPerPage);  // Calculate the total pages
       </div>
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="bootstrap-offline/js/bootstrap.bundle.min.js"></script>
+    <script src="/dara/js/jquery-3.6.0.min.js"></script>
     <script src="/dara/js/products.js"></script>
   </body>
 </html>
